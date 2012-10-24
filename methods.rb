e8f5785716eaa727dac0d21ead3b5c7440d04a94
Coleman Foley <colemanfoley@gmail.com>
2012-10-24 13:13:37 -0700

    Processed filename for easy reading

--- methods.rb
+++ methods.rb
@@ -1,5 +1,17 @@
 def list_people
-  return Dir.entries("people")
+  filenames = Dir.entries("people")
+  names_list = []
+  filenames.each do |filename|
+    filename = filename[0..-6]
+    filename = filename.sub("_", " ")
+    filename_array = filename.split()
+    first_name = String.try_convert(filename_array[0])
+    last_name = String.try_convert(filename_array[1])
+    first_name.capitalize!
+    last_name.capitalize!
+    names_list.push(first_name)
+  end
+  return names_list
 end
 
 def show_person
